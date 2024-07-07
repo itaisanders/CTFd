748b078e212f50a6a9423483d9fb0404d74555de
Itai Sanders <itai_sanders@amat.com>
2024-07-07 18:08:23 +0300

    updated run-dockerized-challenges.py to use arguments for root dir and command

--- HackyEaster/run-dockerized-challenges.py
+++ HackyEaster/run-dockerized-challenges.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
+import sys
 import time
-def run_docker_compose_up(root_dir):
+def run_docker_compose_up(root_dir, command):
     processes = []
     count = 0
     for root, dirs, files in os.walk(root_dir):
@@ -9,7 +10,7 @@ def run_docker_compose_up(root_dir):
             if file == 'docker-compose.yaml':
                 compose_file = os.path.join(root, file)
                 print(f"Found docker-compose file: {compose_file}")
-                cmd = f"docker-compose -f {compose_file} up"
+                cmd = f"sudo docker-compose -f {compose_file} {command}"
                 print(f"Running command: {cmd}")
                 p = subprocess.Popen(cmd, shell=True)
                 processes.append(p)
@@ -24,8 +25,13 @@ def run_docker_compose_up(root_dir):
     print("All docker-compose commands completed.")
 
 if __name__ == '__main__':
-    root_dir = '/workspaces/CTFd/HackyEaster/2021/'   # replace with the path to the folder you want to search in
+    root_dir = './'
+    if len(sys.argv) > 1:
+        root_dir = sys.argv[1]   # replace with the path to the folder you want to search in
+    command = 'up'
+    if len(sys.argv) > 2:
+        command = sys.argv[2]
     start_time = time.time()
-    run_docker_compose_up(root_dir)
+    run_docker_compose_up(root_dir, command)
     end_time = time.time()
     print(f"Script completed in {end_time - start_time:.2f} seconds")
